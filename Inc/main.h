2df266f158254648085aaf912c0f60b7f236b518
ralph-pruitt76 <ralph@weathercloud.co>
2017-10-16 08:53:54 -0600

    Updated Micro Tags to reflect new release.

--- Inc/main.h
+++ Inc/main.h
@@ -15,10 +15,10 @@
 /* Definition for USARTx's NVIC */
 #define USARTx_IRQn        USART3_IRQn
 #define BRD_REV         "Rev M"               // PCB Revision          
-#define VERSION_NUM     "N.8.14"                 // Monitor Revision
-#define REL_DATE        "Oct 13, 2017"
+#define VERSION_NUM     "N.9.0"                 // Monitor Revision
+#define REL_DATE        "Oct 16, 2017"
 //#define LEGACY_BANNER   "Rev G+ REV C"        // OLD.....Needed to allow Legacy Design to work
-#define LEGACY_BANNER   "N8.14 10/13/17"        // Needed to allow Legacy Design to work
+#define LEGACY_BANNER   "N.9.0 10/16/17"        // Needed to allow Legacy Design to work
 
 /* Prototypes */
 //int isHexNum(char *ptr);
